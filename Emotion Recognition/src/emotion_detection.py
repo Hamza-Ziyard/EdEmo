65b13a7c16b4787b680fe4fc2d818e607e2e791f
Hiru1234 <hiruni.2019158@iit.ac.lk>
2021-02-23 08:09:08 +0530

    Updates emotion_detection.py

--- Emotion Recognition/src/emotion_detection.py
+++ Emotion Recognition/src/emotion_detection.py
@@ -1,9 +1,37 @@
 import cv2
+import numpy as np
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Conv2D
+from tensorflow.keras.layers import MaxPooling2D
+from tensorflow.keras.layers import Dense, Dropout, Flatten
+
+# Creating the edEmoModel
+edEmoModel = Sequential()
+
+# Defining the convolutional layers of the cnn model
+edEmoModel.add(Conv2D(32, kernel_size=(3, 3), activation='relu', input_shape=(48, 48, 1)))
+edEmoModel.add(Conv2D(64, kernel_size=(3, 3), activation='relu'))
+edEmoModel.add(MaxPooling2D(pool_size=(2, 2)))
+edEmoModel.add(Dropout(0.25))
+
+edEmoModel.add(Conv2D(128, kernel_size=(3, 3), activation='relu'))
+edEmoModel.add(MaxPooling2D(pool_size=(2, 2)))
+edEmoModel.add(Conv2D(128, kernel_size=(3, 3), activation='relu'))
+edEmoModel.add(MaxPooling2D(pool_size=(2, 2)))
+edEmoModel.add(Dropout(0.25))
+
+edEmoModel.add(Flatten())
+edEmoModel.add(Dense(1024, activation='relu'))
+edEmoModel.add(Dropout(0.5))
+edEmoModel.add(Dense(7, activation='softmax'))
 
 # Facial detection using haarcascade classifiers
 
 # loading haarcascade classifiers
 face_cascade = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
+
+# dictionary which assigns each label an emotion (alphabetical order)
+emotion_dictionary = {0: "Angry", 1: "Disgusted", 2: "Fearful", 3: "Happy", 4: "Neutral", 5: "Sad", 6: "Surprised"}
 
 # Start webcam feed
 video_capture = cv2.VideoCapture(0)
@@ -13,15 +41,23 @@ while True:
     ret, frame = video_capture.read()
     if not ret:
         break
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    # converting rgb to grayscale
+    grayscaleImages = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     faces = face_cascade.detectMultiScale(
-        gray,
+        grayscaleImages,
         scaleFactor=1.3,
         minNeighbors=5)
 
     # Draw rectangle
     for (x, y, w, h) in faces:
         cv2.rectangle(frame, (x, y - 50), (x + w, y + h + 10), (255, 0, 0), 2)
+        roi_gray = grayscaleImages[y:y + h, x:x + w]
+        cropped_img = np.expand_dims(np.expand_dims(cv2.resize(roi_gray, (48, 48)), -1), 0)
+        prediction = edEmoModel.predict(cropped_img)
+        maximumIndex = int(np.argmax(prediction))
+        cv2.putText(frame, emotion_dictionary[maximumIndex], (x + 20, y - 60), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2,
+                    cv2.LINE_AA)
 
     # Display frame
     cv2.imshow('Video', frame)
