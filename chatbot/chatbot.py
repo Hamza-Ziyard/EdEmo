de85cd2315e25c0e28c85acaf5f1b021ef2bb700
DESKTOP-7N6EK83\Ginu <ginura12345@gmail.com>
2021-03-02 12:50:42 +0530

    Added command line chatbot

---
+++ chatbot/chatbot.py
@@ -0,0 +1,23 @@
+from tkinter import *
+def send():
+    send = "You:"+ e.get()
+    text.insert(END,"\n" + send)
+    if(e.get()=='hi'):
+        text.insert(END, "\n" + "Bot: hello! How can I help you?")
+    elif(e.get()=='hello'):
+        text.insert(END, "\n" + "Bot: hi! How can I help you?")
+    elif (e.get() == 'how are you?'):
+        text.insert(END, "\n" + "Bot: i'm fine and you?")
+    elif (e.get() == "i'm fine too"):
+        text.insert(END, "\n" + "Bot: nice to hear that")
+    else:
+        text.insert(END, "\n" + "Bot: Sorry I didnt get it.")
+root = Tk()
+text = Text(root,bg='light blue')
+text.grid(row=0,column=0,columnspan=2)
+e = Entry(root,width=80)
+send = Button(root,text='Send',bg='blue',width=20,command=send).grid(row=1,column=1)
+e.grid(row=1,column=0)
+root.title('EdEmo CHATBOT')
+root.mainloop()
+
