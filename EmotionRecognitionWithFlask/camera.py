6148bc5452636fef248cbc15aa535d6cbb5afba2
Hiru1234 <hsembakutti@gmail.com>
2021-04-18 14:30:24 +0530

    Modified project to measure the percentage of emotion detected

--- EmotionRecognitionWithFlask/camera.py
+++ EmotionRecognitionWithFlask/camera.py
@@ -5,6 +5,7 @@ from tensorflow.keras.layers import MaxPooling2D
 from tensorflow.keras.layers import Dense, Dropout, Flatten
 from tensorflow.keras.optimizers import Adam
 import numpy as np
+import time
 
 ds_factor=0.6
 
@@ -31,6 +32,7 @@ face_cascade=cv2.CascadeClassifier("haarcascade_frontalface_alt2.xml")
 edEmoModel.load_weights('model.h5')
 emotion_dictionary = {0: "Angry", 1: "Disgusted", 2: "Fearful", 3: "Happy", 4: "Neutral", 5: "Sad", 6: "Surprised"}
 
+
 class VideoCamera(object):
     def __init__(self):
         self.video = cv2.VideoCapture(0)
@@ -43,14 +45,49 @@ class VideoCamera(object):
         image=cv2.resize(image,None,fx=ds_factor,fy=ds_factor,interpolation=cv2.INTER_AREA)
         gray=cv2.cvtColor(image,cv2.COLOR_BGR2GRAY)
         face_rects=face_cascade.detectMultiScale(gray,1.3,5)
+        start_time = time.time()
+
+        # data structures
+        time_data = np.array([0])
+        prediction_data = np.array([0])
+
         for (x,y,w,h) in face_rects:
-        	#cv2.rectangle(image,(x,y),(x+w,y+h),(0,255,0),2)
-            cv2.rectangle(image, (x, y - 50), (x + w, y + h + 10), (255, 0, 0), 2)
+            """cv2.rectangle(image, (x, y - 50), (x + w, y + h + 10), (255, 0, 0), 2)
             roi_gray = gray[y:y + h, x:x + w]
             cropped_img = np.expand_dims(np.expand_dims(cv2.resize(roi_gray, (48, 48)), -1), 0)
             prediction = edEmoModel.predict(cropped_img)
             maximumIndex = int(np.argmax(prediction))
-            cv2.putText(image, emotion_dictionary[maximumIndex], (x + 20, y - 60), cv2.FONT_HERSHEY_SIMPLEX, 1,(255, 255, 255), 2,cv2.LINE_AA)
+            cv2.putText(image, emotion_dictionary[maximumIndex], (x + 20, y - 60), cv2.FONT_HERSHEY_SIMPLEX, 1,(255, 255, 255), 2,cv2.LINE_AA)"""
+            cv2.rectangle(image, (x, y - 50), (x + w, y + h + 10), (255, 0, 0), 2)
+            roi_gray = gray[y:y + h, x:x + w]
+            np.set_printoptions(formatter={'float_kind': '{:f}'.format})
+            cropped_img = np.expand_dims(np.expand_dims(cv2.resize(roi_gray, (48, 48)), -1), 0)
+            prediction = edEmoModel.predict(cropped_img)
+
+            # Calculating the prediction percentage
+            maxindex = int(np.argmax(prediction))
+            data = cropped_img.astype('float32')
+            data /= 255
+            probabities = edEmoModel.predict_proba(data, verbose=1)[0]
+            max_string = str(probabities[maxindex].item() * 100)
+            first_three = ' '+max_string[0:3] + '%'
+
+            # Run timer
+            current_time = round(time.time()-start_time,2)
+            time.sleep(0.25)
+            time_data = np.append(time_data,current_time)
+
+            if maxindex in [3,5]:
+                prediction_data = np.append(prediction_data, probabities[maxindex].item() * 100)
+                print('enagaged')
+            else:
+                prediction_data = np.append(prediction_data,probabities[maxindex].item() * -100)
+                print('not enageged')
+
+
+            cv2.putText(image, emotion_dictionary[maxindex] + first_three, (x + 20, y - 60), cv2.FONT_HERSHEY_SIMPLEX, 1,
+                        (255, 255, 255), 2, cv2.LINE_AA)
             break
         ret, jpeg = cv2.imencode('.jpg', image)
         return jpeg.tobytes()
+
--- EmotionRecognitionWithFlask/main.py
+++ EmotionRecognitionWithFlask/main.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('views/room.ejs')
+    return render_template('index.html')
 
 def gen(camera):
     while True:
