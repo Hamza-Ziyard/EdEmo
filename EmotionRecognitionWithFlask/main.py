3b8d7edb9aec1577f14cd1e10e4785eeacbd38fb
Suhith Nanayakkara <suhith42@gmail.com>
2021-04-19 19:18:04 +0530

    Added plotting and displaying results graph functionalities

--- EmotionRecognitionWithFlask/main.py
+++ EmotionRecognitionWithFlask/main.py
@@ -1,12 +1,16 @@
-
-from flask import Flask, render_template, Response
+from flask import Flask, render_template, Response, make_response
 from camera import VideoCamera
 
 app = Flask(__name__)
 
+video_cam = VideoCamera()
+
+
+
 @app.route('/')
 def index():
     return render_template('index.html')
+
 
 def gen(camera):
     while True:
@@ -14,10 +18,22 @@ def gen(camera):
         yield (b'--frame\r\n'
                b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
 
+
+@app.route('/results')
+def display_graph():
+    # file_name = video_cam.plot_graph()
+    file_name = video_cam.plot_graph()
+    # return render_template("results.html", graph="helloooooooo")
+    return render_template('results.html', graph=file_name)
+
+
+
+
 @app.route('/video_feed')
 def video_feed():
-    return Response(gen(VideoCamera()),
+    return Response(gen(video_cam),
                     mimetype='multipart/x-mixed-replace; boundary=frame')
+
 
 if __name__ == '__main__':
     app.run(port=5000, debug=True)
